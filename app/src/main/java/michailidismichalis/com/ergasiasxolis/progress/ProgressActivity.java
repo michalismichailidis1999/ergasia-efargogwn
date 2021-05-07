a65869a2a6c4ad55785a4ae01262647a82606a84
nikgeokar <nikgeokar@csd.auth.gr>
2021-05-07 23:22:15 +0300

    Ξεκίνησα το ProgressActivity

--- app/src/main/java/michailidismichalis/com/ergasiasxolis/progress/ProgressActivity.java
+++ app/src/main/java/michailidismichalis/com/ergasiasxolis/progress/ProgressActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -37,7 +37,7 @@ public class ProgressActivity extends AppCompatActivity {
 
         Mealist= new ArrayList<>();
         fetchMeals();
-        System.out.println("hi");
+        System.out.println("hiii");
         System.out.print(Mealist.size());
 
         //MealHistory.setVisibility(View.GONE);
