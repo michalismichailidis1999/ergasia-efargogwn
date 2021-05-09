821a802500642024951e5e7d539d9b2ecf900510
nikgeokar <nikgeokar@csd.auth.gr>
2021-05-09 16:05:00 +0300

    Ξεκίνησα το ProgressActivity2

--- app/src/main/java/michailidismichalis/com/ergasiasxolis/Data/Data.java
+++ app/src/main/java/michailidismichalis/com/ergasiasxolis/Data/Data.java
@@ -16,7 +16,7 @@ public class Data {
     private void createData(){
         foodList.add(new FoodObject("1", "Meat", "Chicken Breast", "https://www.spendwithpennies.com/wp-content/uploads/2018/08/SpendWithPennies-Oven-Baked-Chicken-Breast-22-500x500.jpg", 169, 31, 4, 0));
         foodList.add(new FoodObject("2", "Meat", "Rise", "https://www.healthweb.gr/wp-content/uploads/2020/12/r2.jpg", 169, 31, 4, 0));
-        foodList.add(new FoodObject("3", "C", "Potatos", "https://5.imimg.com/data5/CL/OX/SB/GLADMIN-2063743/selection-020-500x500.png", 180, 1, 4, 40));
+        foodList.add(new FoodObject("3", "Vegetables", "Potatos", "https://5.imimg.com/data5/CL/OX/SB/GLADMIN-2063743/selection-020-500x500.png", 180, 1, 4, 40));
         foodList.add(new FoodObject("4", "Meat", "Steak", "https://www.siakos.gr/340-large_default/strip-loin-steak-moscharisio-ellados.jpg", 169, 31, 4, 0));
     }
 
